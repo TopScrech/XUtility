f470976af380505bea0456146fa0f986cf9bf30c
TopScrech <sergei_saliukov@icloud.com>
2024-11-27 12:43:12 +0100

    Initial Commit

---
+++ XUtility/XUtilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  XUtilityApp.swift
+//  XUtility
+//
+//  Created by Sergei Saliukov on 27/11/24.
+//
+
+import SwiftUI
+
+@main
+struct XUtilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
